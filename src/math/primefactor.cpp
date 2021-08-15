f47c1e7367b2704a54481032cdd32fa18c429d03
Ryoga.exe <ryoga.dev@gmail.com>
2021-08-15 23:41:10 +0900

    [math] Add: primefactor.cpp

---
+++ src/math/primefactor.cpp
@@ -0,0 +1,15 @@
+#include "base.hpp"
+
+vector<pair<ll, ll>> prime_factorize(ll n) {
+    vector<pair<ll, ll>> res;
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i != 0) continue;
+        ll ex = 0;
+        while (n % i == 0) {
+            ++ex; n /= i;
+        }
+        res.push_back({ i, ex });
+    }
+    if (n != 1) res.push_back({ n, 1 });
+    return res;
+}
