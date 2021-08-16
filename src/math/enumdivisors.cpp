1ec771d3071bb86061c3d6f81a4e0ad0b44dc686
Ryoga.exe <ryoga.dev@gmail.com>
2021-08-16 16:05:21 +0900

    [math] Add: enumdivisors.cpp

---
+++ src/math/enumdivisors.cpp
@@ -0,0 +1,13 @@
+#include "base.hpp"
+
+vector<ll> enum_divisors(ll n) {
+    vector<ll> res;
+    for (ll i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (n/i != i) res.push_back(n/i);
+        }
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
