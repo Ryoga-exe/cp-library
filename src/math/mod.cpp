f67e8c6d4bee1d7bac67ecbda48b5cff56b3d0a1
Ryoga.exe <ryoga.dev@gmail.com>
2021-08-16 16:04:48 +0900

    [math] Add: mod.cpp

---
+++ src/math/mod.cpp
@@ -0,0 +1,25 @@
+#include "base.hpp"
+
+ll modPow(ll a, ll n, ll p) {
+    if (n == 0) return 1;
+    if (n == 1) return a % p;
+    if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p;
+    ll t = modPow(a, n / 2, p);
+    return (t * t) % p;
+}
+
+ll modInv(ll a, ll p) {
+    return modPow(a, p - 2, p);
+}
+
+ll modInvE(ll a, ll m) {
+    ll b = m, u = 1, v = 0;
+    while (b) {
+        ll t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m; 
+    if (u < 0) u += m;
+    return u;
+}
