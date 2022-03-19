221745bb590e352010c5bf67d6d892d61cb294be
Ryoga.exe <ryoga.dev@gmail.com>
2022-03-20 01:42:51 +0900

    Update template

--- src/template.cpp
+++ src/template.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 using ll = long long;
 using Graph2 = vector<vector<int>>;
-constexpr int INF32 = 1<<30;
+constexpr int INF32 = 1<<29;
 constexpr ll INF64 = 1LL<<60;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
