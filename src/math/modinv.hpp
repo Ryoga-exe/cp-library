3ceafee8f2f97e4ab420e30f5e295dd916e8fb26
Ryoga.exe <ryoga.dev@gmail.com>
2022-10-22 19:12:32 +0900

    [math] add: modinv

---
+++ src/math/modinv.hpp
@@ -0,0 +1,18 @@
+#include <utility>
+#include "modpow.hpp"
+
+/// start
+/// @prefix cpModInv
+/// @description ModInv : mod p での a の逆元 (a^{-1}) を計算します \n O(log(p))
+/// @isFileTemplate false
+long long ModInv(long long a, long long p) {
+    long long b = p, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; std::swap(a, b);
+        u -= t * v; std::swap(u, v);
+    }
+    u %= p; 
+    if (u < 0) u += p;
+    return u;
+}
