0502d11551074b8e510a46a2a4a9840ed76874d2
Ryoga.exe <ryoga.dev@gmail.com>
2022-10-22 19:18:12 +0900

    [math] update: primefactorize

--- src/math/primefactorize.hpp
+++ src/math/primefactorize.hpp
@@ -5,7 +5,7 @@
 /// @prefix cpPrimeFactorize
 /// @description Prime factorization of n : n を素因数分解します \n O(sqrt(N))
 /// @isFileTemplate false
-std::vector<std::pair<long long, long long>> PrimeFactorize(long long n) {
+std::vector<std::pair<long long, long long>> PrimeFactorize(long long n) noexcept {
     std::vector<std::pair<long long, long long>> res;
     for (long long i = 2; i * i <= n; i++) {
         if (n % i != 0) continue;
