5ff00696b51c0f523f5b7c40bec45e4488bf6ad3
Ryoga.exe <ryoga.dev@gmail.com>
2022-10-22 20:04:07 +0900

    [graph] update

--- src/graph/weightedgraph.hpp
+++ src/graph/weightedgraph.hpp
@@ -1,5 +1,9 @@
 #include <vector>
 
+/// start
+/// @prefix cpWeightedGraph
+/// @description WeightedGraph : 重み付きグラフ
+/// @isFileTemplate false
 struct Edge {
     Edge(long long _to, long long _w) : to(_to), w(_w) {}
     long long to, w;
