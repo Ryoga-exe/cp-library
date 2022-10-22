0609bf83c2056aa0963426e0b95c0cd50a0110b2
Ryoga.exe <ryoga.dev@gmail.com>
2022-10-22 18:10:14 +0900

    [utils] update: second_compare

---
+++ src/utils/second_compare.hpp
@@ -0,0 +1,12 @@
+#include <utility>
+
+/// start
+/// @prefix cpSecondCompare
+/// @description std::pair の second で比較を行う関数オブジェクト
+/// @isFileTemplate false
+struct second_compare {
+    template<typename T, typename U>
+    inline bool operator() (const std::pair<T, U> a, const std::pair<T, U> b) const noexcept {
+        return (a.second != b.second ? a.second < b.second : a.first < b.first);
+    }
+};
