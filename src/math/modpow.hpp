6001af3eddb21f180a222f121da80763f8e5d5dd
Ryoga.exe <ryoga.dev@gmail.com>
2022-10-22 19:08:30 +0900

    [math] add: modpow

---
+++ src/math/modpow.hpp
@@ -0,0 +1,12 @@
+/// @prefix cpModPow
+/// @description ModPow : 高速に a ^ n mod p を計算します  \n O(log(n))
+/// @isFileTemplate false
+long long ModPow(long long a, long long n, long long p) noexcept {
+    long long res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % p;
+        a = a * a % p;
+        n >>= 1;
+    }
+    return res;
+}
