e889e94d5f4ba2a411ddce793fe86d4d253714c5
Ryoga.exe <ryoga.dev@gmail.com>
2022-10-24 02:40:57 +0900

    [data-structure] update rangeset.hpp: impl insert and erase

--- src/data-structure/rangeset.hpp
+++ src/data-structure/rangeset.hpp
@@ -1,6 +1,9 @@
 #include <set>
 #include <utility>
+#include <iterator>
 #include <vector>
+#include <cassert>
+#include <algorithm>
 
 /// start
 /// @prefix cpRangeSet
@@ -49,10 +52,50 @@ public:
         return result;
     }
     std::pair<iterator, bool> insert(const value_type left, const value_type right) {
-        // todo
+        assert(left <= right);
+        if (left == right) {
+            return { m_set.end(), false };
+        }
+        auto itr_l = m_set.upper_bound({ left, left });
+        auto itr_r = m_set.upper_bound({ right, right });
+        if (itr_l != m_set.begin()) {
+            if (std::prev(itr_l)->second >= left) {
+                itr_l--;
+            }
+        }
+        auto tl = left, tr = right;
+        if (itr_l != itr_r) {
+            tl = std::min(left, itr_l->first);
+            tr = std::min(right, std::prev(itr_r)->second);
+            m_set.erase(itr_l, itr_r);
+        }
+        m_set.insert({ tl, tr });
+        return { itr_l, true };
     }
     void erase(const value_type left, const value_type right) {
-        // todo
+        assert(left <= right);
+        if (left == right) {
+            return;
+        }
+        auto itr_l = m_set.upper_bound({ left, left });
+        auto itr_r = m_set.upper_bound({ right, right });
+        if (itr_l != m_set.begin()) {
+            if (std::prev(itr_l)->second >= left) {
+                itr_l--;
+            }
+        }
+        if (itr_l == itr_r) {
+            return;
+        }
+        auto tl = std::min(left, itr_l->first);
+        auto tr = std::max(right, std::prev(itr_r)->second);
+        m_set.erase(itr_l, itr_r);
+        if (tl < left) {
+            m_set.insert({ tl, left });
+        }
+        if (right < tr) {
+            m_set.insert({ right, tr });
+        }
     }
     iterator find(const value_type left, const value_type right) const {
         // todo
