833503fa018d8000416ecd4f180d0baf2d732c8e
Ryoga.exe <ryoga.dev@gmail.com>
2022-10-25 16:06:23 +0900

    [debug] update: #12

--- src/debug/debug.hpp
+++ src/debug/debug.hpp
@@ -1,8 +1,8 @@
 /// @prefix cpDebug
 /// @description Debug Print : デバッグプリント用スニペット
 /// @isFileTemplate false
-#if __has_include("../src/debug/print.hpp")
-    #include "../src/debug/print.hpp"
+#if __has_include("../src/debug/debug_print/debug_print.hpp")
+    #include "../src/debug/debug_print/debug_print.hpp"
     #define $(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
 #else
     #define $(...) 0
