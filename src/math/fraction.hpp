cc4528ca4cd4142bf7f0d44b67948c0a34f267eb
Ryoga.exe <ryoga.dev@gmail.com>
2022-10-30 14:11:27 +0900

    [math] refactor: fraction

--- src/math/fraction.hpp
+++ src/math/fraction.hpp
@@ -12,53 +12,26 @@ public:
     using traits_type = double;
     using pair_type = std::pair<value_type, value_type>;
 private:
-    value_type m_num = 1, m_den = 1;
-    inline constexpr value_type inner_sign(const value_type x) const {
-        return (0 < x) - (x < 0);
-    }
+    value_type m_num = 0, m_den = 1;
+    inline constexpr value_type inner_sign(const value_type x) const noexcept { return (0 < x) - (x < 0); }
+    inline constexpr value_type inner_abs(const value_type x) const noexcept { return (x < 0 ? -x : x); }
 public:
-    Fraction() = default;
-    Fraction(const Fraction&) = default;
-    Fraction(Fraction&&) = default;
-    explicit constexpr Fraction(value_type num, value_type den) noexcept {
+    Fraction() noexcept = default;
+    Fraction(const Fraction&) noexcept = default;
+    Fraction(Fraction&&) noexcept = default;
+    explicit constexpr Fraction(const value_type num, const value_type den) noexcept {
         auto g = std::gcd(num, den);
-        m_num = inner_sign(num) * inner_sign(den) * std::abs(num) / g;
-        m_den = std::abs(den) / g;
+        m_num = inner_sign(num) * inner_sign(den) * inner_abs(num) / g;
+        m_den = inner_abs(den) / g;
     }
-    explicit constexpr Fraction(value_type x) noexcept {
-        m_num = x;
-        m_den = 1;
-    }
+    explicit constexpr Fraction(const value_type x) noexcept { m_num = x; m_den = 1; }
     constexpr Fraction& operator =(const Fraction&) noexcept = default;
     constexpr Fraction& operator =(Fraction&&) noexcept = default;
     inline constexpr value_type num() const noexcept { return m_num; }
     inline constexpr value_type den() const noexcept { return m_den; }
     inline constexpr pair_type asPair() const noexcept { return pair_type{ m_num, m_den }; }
-    explicit inline operator traits_type() const noexcept { return (m_den == inner_sign(m_num) * static_cast<traits_type>(1LL << 60) ? : static_cast<traits_type>(m_num) / m_den); }
-    Fraction& operator +=(const Fraction& rhs) noexcept {
-        m_num = m_num * rhs.m_den + rhs.m_num * m_den; m_den = m_den * rhs.m_den;
-        auto g = std::gcd(m_num, m_den);
-        m_num /= g; m_den /= g;
-        return *this;
-    }
-    Fraction& operator -=(const Fraction& rhs) noexcept {
-        m_num = m_num * rhs.m_den - rhs.m_num * m_den; m_den = m_den * rhs.m_den;
-        auto g = std::gcd(m_num, m_den);
-        m_num /= g; m_den /= g;
-        return *this;
-    }
-    Fraction& operator *=(const Fraction& rhs) noexcept {
-        m_num = m_num * rhs.m_num; m_den = m_den * rhs.m_den;
-        auto g = std::gcd(m_num, m_den);
-        m_num /= g; m_den /= g;
-        return *this;
-    }
-    Fraction& operator /=(const Fraction& rhs) noexcept {
-        m_num = m_num * rhs.m_den; m_den = m_den * rhs.m_num;
-        auto g = std::gcd(m_num, m_den);
-        m_num /= g; m_den /= g;
-        return *this;
-    }
+    inline constexpr Fraction inv() const noexcept { return Fraction{ m_den, m_num }; }
+    explicit inline constexpr operator traits_type() const noexcept { return (m_den == 0 ? inner_sign(m_num) * static_cast<traits_type>(1LL << 60) : static_cast<traits_type>(m_num) / m_den); }
     friend inline constexpr Fraction operator +(const Fraction& lhs, const Fraction& rhs) noexcept { return Fraction{ lhs.m_num * rhs.m_den + rhs.m_num * lhs.m_den, lhs.m_den * rhs.m_den }; }
     friend inline constexpr Fraction operator -(const Fraction& lhs, const Fraction& rhs) noexcept { return Fraction{ lhs.m_num * rhs.m_den - rhs.m_num * lhs.m_den, lhs.m_den * rhs.m_den }; }
     friend inline constexpr Fraction operator *(const Fraction& lhs, const Fraction& rhs) noexcept { return Fraction{ lhs.m_num * rhs.m_num, lhs.m_den * rhs.m_den }; }
@@ -69,4 +42,8 @@ public:
     friend inline constexpr bool operator  >(const Fraction& lhs, const Fraction& rhs) noexcept { return rhs < lhs; }
     friend inline constexpr bool operator <=(const Fraction& lhs, const Fraction& rhs) noexcept { return !(lhs > rhs); }
     friend inline constexpr bool operator >=(const Fraction& lhs, const Fraction& rhs) noexcept { return !(lhs < rhs); }
+    Fraction& operator +=(const Fraction& rhs) noexcept { *this = *this + rhs; return *this; }
+    Fraction& operator -=(const Fraction& rhs) noexcept { *this = *this - rhs; return *this; }
+    Fraction& operator *=(const Fraction& rhs) noexcept { *this = *this * rhs; return *this; }
+    Fraction& operator /=(const Fraction& rhs) noexcept { *this = *this / rhs; return *this; }
 };
